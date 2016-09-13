14173387a6b5720c611b0c7aec30d7525db054ae
Emr03 <elsa.riachi@mail.mcgill.ca>
2016-09-13 19:12:31 -0400

    Add system timer code sample

---
+++ timer/Debug/main.d
@@ -0,0 +1,17 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/elsa/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
+main.obj: /home/elsa/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h
+main.obj: /home/elsa/ti/SW-TM4C-2.1.3.156/inc/hw_types.h
+main.obj: /home/elsa/ti/SW-TM4C-2.1.3.156/inc/hw_memmap.h
+main.obj: /home/elsa/ti/SW-TM4C-2.1.3.156/driverlib/sysctl.h
+main.obj: /home/elsa/ti/SW-TM4C-2.1.3.156/driverlib/gpio.h
+
+../main.c: 
+/home/elsa/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
+/home/elsa/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdbool.h: 
+/home/elsa/ti/SW-TM4C-2.1.3.156/inc/hw_types.h: 
+/home/elsa/ti/SW-TM4C-2.1.3.156/inc/hw_memmap.h: 
+/home/elsa/ti/SW-TM4C-2.1.3.156/driverlib/sysctl.h: 
+/home/elsa/ti/SW-TM4C-2.1.3.156/driverlib/gpio.h: 
